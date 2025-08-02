cffdc0a605019939431c754e4329b7c8f45c66ef
Diana <dvizh1203@gmail.com>
2025-08-02 16:41:46 +0300

    Update middleware.py

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -1,1 +1,6 @@
+from websockets.server import WebSocketServerProtocol
 
+async def logging_middleware(websocket: WebSocketServerProtocol, message: str):
+    """Логирование входящих сообщений."""
+    print(f"Получено сообщение от {websocket.remote_address}: {message}")
+    return message
